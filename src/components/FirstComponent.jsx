e5c9de50f483804cb0b56ea8acad098e365d734e
Hồ Hoàng Sơn <hoson2k5@gmail.com>
2025-12-17 20:51:58 +0700

    Exercise 1: Create FirstComponent - basic functional component

---
+++ src/components/FirstComponent.jsx
@@ -0,0 +1,13 @@
+// Exercise 1: Creating Your First Component
+// This is a simple functional component that renders JSX
+
+function FirstComponent() {
+  return (
+    <div className="first-component">
+      <h1>Hello, React!</h1>
+      <p>This is my first React component.</p>
+    </div>
+  );
+}
+
+export default FirstComponent;
